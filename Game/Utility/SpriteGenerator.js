5e56f9bc50601d62ee02f347913724f176827c2a
Sascha <saschakoenninger@gmail.com>
2020-02-13 14:34:37 +0100

    added goblin get hit animation

--- Game/Utility/SpriteGenerator.js
+++ Game/Utility/SpriteGenerator.js
@@ -184,6 +184,11 @@ var Game;
                         sprite.generateByGrid(textureImage, fudge.Rectangle.GET(50, 60, 50, 50), 8, new fudge.Vector2(100, 0), 32, fudge.ORIGIN2D.CENTER);
                         return sprite;
                     }
+                    case Game.CHARACTERSTATE.HIT: {
+                        let sprite = new Sprite(spriteName + "_" + stateName);
+                        sprite.generateByGrid(textureImage, fudge.Rectangle.GET(50, 60, 50, 50), 4, new fudge.Vector2(100, 0), 32, fudge.ORIGIN2D.CENTER);
+                        return sprite;
+                    }
                 }
             }
             case "blob": {
